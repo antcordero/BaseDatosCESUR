750685c547cc486c96af39728c81e12320c544ea
Antonio Cordero Molina <antoniocorderomn@gmail.com>
2024-12-18 08:35:57 +0100

    18/12/2024

--- CreacionTablas/Ejercicios/Ejercicio3.sql
+++ CreacionTablas/Ejercicios/Ejercicio3.sql
@@ -24,13 +24,13 @@ use liga_EA;
 
 create table jugador (
 	dni varchar(9) primary key constraint chk_dni check(char_length(dni)=9),
-    nombre varchar(20) constraint chk_nombre check (char_length(nombre)>=10 and char_length(nombre)<=20),
+    nombre varchar(20) constraint chk_nombre check (char_length(nombre)>=10),
     apellido varchar(20),
-    altura decimal(2) constraint chk_altura check(altura>=1.50 and altura <=2.10),
-    posicion enum ("portero","defensa","medio","delatero","No_Definido") default 'No_Definido',
+    altura decimal(3,2) constraint chk_altura check(altura between 1.50 and 2.10),
+    posicion enum ("portero","defensa","medio","delantero","No_Definido") default 'No_Definido',
     fecha_nacimiento date constraint chk_fecha check(month(fecha_nacimiento) between 3 and 10),
-    email varchar(20) constraint chk_email check(email like "%@%"),
-    direccion varchar(100) constraint chk_dir check (direccion in ("Calle%", "Avenida%")),
+    email varchar(30) constraint chk_email check(email like "%@%"),
+    direccion varchar(100) constraint chk_dir check (direccion like "Calle%" or direccion like "Avenida%"),
     clave varchar(15) constraint chk_clave check(char_length(clave)>=8)
 );
 
@@ -38,10 +38,7 @@ create table jugador (
 todas las restricciones se han creado de forma correcta.*/
 
 insert into jugador(dni, nombre, apellido, altura, posicion, fecha_nacimiento, email, direccion, clave) values
-	("12345678A", "Cristiano", "Ronaldo", 1.90, "delantero", 1985-07-07, "cr7@gmail.com", "Calle El Bicho", "12345678CR7"),
-    ("12345678B", "Leo", "Messi", 1.60, "delantero", 1986-07-07, "messi10@gmail.com", "Calle El Bicho", "12345678MES10")
-;
+	("12345678A", "abcdefgheijs", "Ronaldo", 1.90, "delantero", "2000-10-10", "cr7@gmail.com", "Calle El Bicho", "12345678CR7");
 
-insert into jugador values
-("1234567C", "Vinicius", "JR", 1.75, 1998-07-07, "vinijr@gmail.com", "Calle El Bicho", "12345687VINIJR");
+select * from jugador
 
